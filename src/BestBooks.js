7344143838aeac03dab23398a62241a6763fcfb8
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-06-01 17:01:42 +0300

    finish-lab11-add-MongoDB

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,49 +1,75 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel'
-import Jumbotron from 'react-bootstrap/Jumbotron';
+// import Carousel from 'react-bootstrap/Carousel'
+// import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
 import axios from 'axios';
+// import Profile from './Profile';
+import { withAuth0 } from '@auth0/auth0-react';
+import Card from 'react-bootstrap/Card';
+
+
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+    imageUrl:'',
+      name: '',
+      description: '',
+      status: '',
       server: process.env.REACT_APP_PORT,
       bookArr: [],
+      showBooks: true
     }
   }
+  componentDidMount = async () => {
+    const user  = this.props.auth0.user.email;
+    const myBooks = `${this.state.server}/books?email=${user}`;
+    const showApiUrlbook = await axios.get(myBooks);
+console.log(showApiUrlbook);
+    this.setState({
+      bookArr: showApiUrlbook.data,
+      showBooks: true
+    });
+console.log(this.state.bookArr)  }
+
 
   render() {
-    return (
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
+    return(
+      <>
+      
+  
+      {this.state.showBooks &&
+     
+        <>
+          {this.state.bookArr.map((item, index) => {
+            return (
+              <>
+                <Card style={{ width: '18rem' }} key={index}>
+                  <Card.Body>
+                  <Card.Img variant="top" src={item.imageUrl} />
 
-        <p>
-          This is a collection of my favorite books
-        </p>
-          <Carousel>
-          {this.state.bookArr.length &&
-            this.state.bookArr.map((item, idx) => {
-              return (
-                <Carousel.Item interval={1000}>
-                  {/* <img
-                    className="d-block w-100"
-                    alt="First slide"
-                  /> */}
-                  <Carousel.Caption>
-                    <h3> {item.name}</h3>
-                    <p>{item.description}</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              )
-            })
-          } 
-         </Carousel> 
+                    <Card.Title>Name: {item.name}</Card.Title>
+                    <Card.Text>Description: {item.description}</Card.Text>
+                    <Card.Text>Status: {item.status}</Card.Text>                         
+                  </Card.Body>
+                </Card>
+              
+              </>
+            );
 
-      </Jumbotron>
+          })
+          }
+        </>
+      }
+
+
+    
+  </>
     )
-  }
 }
+}
+    
 
-export default BestBooks;
+export default withAuth0(BestBooks);
